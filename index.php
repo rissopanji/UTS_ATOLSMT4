fbbb5d0ccb54e4199989619fd589a54057bdcfe6
rissopanji <rissopanji156@gmail.com>
2022-06-04 22:57:18 +0700

    Update Index

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Data Mahasiswa</title>
+
+  <title>UTS APLIKASI DAN TEKNOLOGI</title>
+
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
   <style>
     .mx-auto {
@@ -21,6 +23,7 @@
 <body>
 
   <?php
+  //Mengambil Data Form
 
   $data = isset($_POST['data']) ? json_decode($_POST['data'], true) : [];
   if (isset($_POST['submit'])) {
@@ -31,25 +34,50 @@
     ];
   } ;
 
-
+  //Data Form Menuju Array Kolom
   $nims = array_column($data, 'nim');
   $namas = array_column($data, 'nama');
   $nilais = array_column($data, 'nilai');
+
+  //Nilai
   $nilai_terbesars = $data;
+  $nilai_terkecils = $data;
 
+  $nilai_ratas = 0;
 
+  $data_sorts = $data;
+
+  
   if (count($data)) {
+
+      //Nilai Terbesar
       usort($nilai_terbesars, function($a, $b) {
           if ($a['nilai'] == $b['nilai']) {
             return 0;
           }
           return ($a['nilai'] > $b['nilai']) ? -1 : 1;
       });
+
+      //Nilai Terkecil
+      usort($nilai_terkecils, function($a, $b) {
+          if ($a['nilai'] == $b['nilai']) {
+              return 0;
+          }
+          return ($a['nilai'] < $b['nilai']) ? -1 : 1;
+      });
+
+      //Mengurutkan Nilai
+      array_multisort($nims, SORT_ASC, $namas, SORT_ASC, $nilais, SORT_ASC, $data_sorts);
+      
+      //Nilai Rata-Rata
+      $nilai_ratas = array_sum($nilais) / count($nilais);
   }
 
   ?>
+
   <div class="mx-auto">
-    <!-- untuk memasukkan data -->
+
+    <!-- Untuk Memasukkan Data -->
     <div class="card">
       <div class="card-header">
         <b>MASUKAN DATA</b>
@@ -91,7 +119,7 @@
         <table class="table">
           <thead>
             <tr>
-              <th scope="col">NIM</th>
+              <th scope="col">Nim</th>
               <th scope="col">Nama</th>
               <th scope="col">Nilai</th>
             </tr>
@@ -120,7 +148,7 @@
         <table class="table">
           <thead>
             <tr>
-              <th scope="col">NIM</th>
+              <th scope="col">Nim</th>
               <th scope="col">Nama</th>
               <th scope="col">Nilai</th>
             </tr>
@@ -138,7 +166,81 @@
       </div>
     </div>
 
-  
+    <!-- Data Diurutkan Dari nilai terkecil -->
+
+    <div class="card">
+      <div class="card-header text-white bg-secondary">
+        Data Mahasiswa Diurutkan Dari Nilai Paling Besar
+      </div>
+      <div class="card-body">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Nim</th>
+              <th scope="col">Nama</th>
+              <th scope="col">Nilai</th>
+            </tr>
+          </thead>
+          <tbody>
+          <?php foreach($nilai_terkecils as $nilai_terkecil) { ?>
+            <tr>
+                <td><?= $nilai_terkecil['nim']?></td>
+                <td><?= $nilai_terkecil['nama']?></td>
+                <td><?= $nilai_terkecil['nilai']?></td>
+            </tr>
+            <?php } ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    <!-- Data Sudah Di urutkan-->
+
+    <div class="card">
+      <div class="card-header text-white bg-secondary">
+        Data Mahasiswa Diurutkan Dari Nim
+      </div>
+      <div class="card-body">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Nim</th>
+              <th scope="col">Nama</th>
+              <th scope="col">Nilai</th>
+            </tr>
+          </thead>
+          <tbody>
+          <?php foreach($data_sorts as $data_sort) { ?>
+            <tr>
+                <td><?= $data_sort['nim'] ?></td>
+                <td><?= $data_sort['nama'] ?></td>
+                <td><?= $data_sort['nilai'] ?></td>
+            </tr>
+            <?php } ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    
+    <div class="card">
+      <div class="card-header text-white bg-secondary">
+        Nilai Rata-Rata
+      </div>
+      <div class="card-body">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Nilai</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+                <td><b><?= $nilai_ratas ?></b></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
 
   </div>
 </body>
