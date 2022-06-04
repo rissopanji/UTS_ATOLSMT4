d00fdcb29361ec4509b2ae2c16894bf37714661c
rissopanji <rissopanji156@gmail.com>
2022-06-04 23:01:47 +0700

    Tambah ICON

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+  <!-- icon address bar -->
+  <link rel="shortcut icon" href="logo_unikom_kuning.png">
+
   <title>UTS APLIKASI DAN TEKNOLOGI</title>
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
